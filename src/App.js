e2699a862dd4813f5729eb208902e94eb05c61fe
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-29 00:27:33 +0600

    header and banner part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h2>sohag</h2>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-center mt-4 mb-4' >My Electronic Grocery</h1>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
