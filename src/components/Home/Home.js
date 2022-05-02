889c02bf707a62d844d4c6e96ad350c99c62dea1
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-05-02 15:29:14 +0600

    extra secion is done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Inventories from '../Inventories/Inventories';
 import Offer from '../Offer/Offer';
+import Review from '../Review/Review';
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-center mt-4 mb-4' style={{ color: 'darkblue' }}>My Electronic Grocery</h1>
+            <h1 className='text-center mt-4 mb-4' style={{ color: 'darkblue' }}>My Electronic Shop</h1>
             <Banner></Banner>
             <Inventories></Inventories>
             <Offer></Offer>
+            <Review></Review>
         </div>
     );
 };
