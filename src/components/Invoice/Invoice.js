16948e6f11556f69a5a49e5f53153b7ac2046dfe
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-05-05 01:11:28 +0600

    digit button click

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -62,7 +62,7 @@ const Invoice = () => {
 
     return (
         <div className='container mt-3 mb-5'>
-            <div className='w-50  mx-auto mt-4 row'  >
+            <div className='w-50 mx-auto mt-4 row'  >
                 <div className=' mb-5 pb-3 col col-lg-12 col-12'>
                     <img width='200px' src={products.img} alt="" />
                     <h5>Name : {products.name}</h5>
@@ -75,8 +75,8 @@ const Invoice = () => {
                 </div>
                 <div className='col col-lg-12 col-12'>
                     <h5 >Enter the Added Number</h5>
-                    <form onSubmit={handleUpdate} className='d-flex pb-5 '>
-                        <input type="number" name="number" id="" placeholder='digit' />
+                    <form onSubmit={handleUpdate} className=' d-flex pb-5 '>
+                        <input style={{ width: '50%' }} type="number" name="number" id="" placeholder='digit' />
                         <input className='bg-info border-1 rounded' type="submit" value="Add" />
                     </form>
                 </div>
